5367eed6c745e07f56b73c598446a3a06ece904a
tomowarkar <35528440+tomowarkar@users.noreply.github.com>
2021-09-22 01:43:23 +0900

    Update data_gen.py

--- data/chalcophaps/data_gen.py
+++ data/chalcophaps/data_gen.py
@@ -3,17 +3,18 @@ import random
 import pandas as pd
 
 random.seed(0)
-rows = 30
+date = pd.date_range("20210901", "20210930", freq="H")
+rows = len(date)
 
 df = pd.DataFrame(
     {
-        "date": pd.date_range("20210901", periods=rows),
+        "date": date,
+        "store": random.choices(["Tokyo", "Osaka"], k=rows, weights=[7, 5]),
         "item": random.choices(["Apple", "Grape", "Orange"], k=rows, weights=[7, 5, 3]),
         "qty": [int(random.gammavariate(alpha=1, beta=2)) for _ in range(rows)],
     }
 )
 
-df = df.query("qty>0")
+df = df.query("qty > 0")
 
 df.to_csv("sample.csv", index=None)
-df.to_excel("sample.xlsx", index=None)
